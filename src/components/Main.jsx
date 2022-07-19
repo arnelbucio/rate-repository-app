0cd8d41e407bc60ecc857d6f33909cc77a5b2747
Arnel Bucio <rnlbucio@gmail.com>
2022-07-19 10:22:19 +0800

    Part 10b Routing

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import { StyleSheet, View } from 'react-native'
+import { Route, Routes, Navigate } from 'react-router-native'
 import RepositoryList from './RepositoryList'
 import AppBar from './AppBar'
+import theme from '../theme';
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: theme.colors.mainBackground,
     flexGrow: 1,
     flexShrink: 1,
-    backgroundColor: '#e1e4e8'
   }
 })
 
@@ -14,7 +16,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path='/' element={<RepositoryList />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
     </View>
   )
 }
