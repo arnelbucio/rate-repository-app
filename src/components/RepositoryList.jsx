30c049c46e7c70ba958fe1fb247373760a88eaf2
Arnel Bucio <rnlbucio@gmail.com>
2022-07-21 10:29:32 +0800

    Part 10d Testing components

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -8,13 +8,9 @@ const styles = StyleSheet.create({
   },
 })
 
-const ItemSeparator = () => <View style={styles.separator} />
-
-const RepositoryList = () => {
-  const { repositories } = useRepositories()
-
+export const RepositoryListContainer = ({ repositories }) => {
   const repositoryNodes = repositories
-    ? repositories.edges.map(edge => edge.node)
+    ? repositories.edges.map((edge) => edge.node)
     : []
 
   const renderItem = ({ item }) => (
@@ -29,6 +25,14 @@ const RepositoryList = () => {
       keyExtractor={item => item.id}
     />
   )
+}
+
+const ItemSeparator = () => <View style={styles.separator} />
+
+const RepositoryList = () => {
+  const { repositories } = useRepositories()
+
+  return <RepositoryListContainer repositories={repositories} />;
 }
 
 export default RepositoryList
